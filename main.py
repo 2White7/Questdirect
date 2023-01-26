e99bf4211f0a808f14fccc4f72c3b6fe01a70107
2White7 <mr.whitening27@gmail.com>
2023-01-26 20:49:27 +0200

    create all function for inteface

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sqlite3
 import datetime
 
+#блок создания таблиц
 with sqlite3.connect("qdiary.db") as db:
     cursor = db.cursor()
 
@@ -24,7 +25,7 @@ with sqlite3.connect("qdiary.db") as db:
     """
 
     cursor.executescript(query)
-
+#функция регистрации пользователя, по задумке можно будет на разных акках чекать разные задачи
 def registration():
     name = input("Введите имя: ")
     login = input("Введите логин: ")
@@ -47,6 +48,11 @@ def registration():
         cursor.close()
         db.close()
 
+# вход в акк
+def account():
+    pass
+
+# функция создания задачи
 def createtask():
     ntask = str(input("Введите название задачи: "))
     dtask = str(input("Введите описание задачи: "))
@@ -56,26 +62,51 @@ def createtask():
     h = int(input("Введите час на который запланирована задача: "))
     m = int(input("Введите минуты для указания точного времени: "))
     date = datetime.datetime(y, mon, d, h, m)
-    h1 = int(input("Сколько часов вы планируете на задачу: "))
-    #if h1 > 25:
-        #print("")
-    m1 = int(input("Сколько минут вы планируете на задачу: "))
+    h1 = int(input("Введите сколько часов вы планируете на задачу: "))
+    #if h1 > 25: проверка на дебила 1(если ввести больше 24 дейттайм крашит нахер)
+        #print("К сожалению в сутках 24 часа")
+    m1 = int(input("Введите сколько минут вы планируете на задачу: "))
+    #if m1 > 60: проверка на дебила 2(суть та же что и в 1 но с минутами)
+        #print("Так нельзя, в одном часу 60 минут")
     long = datetime.time(h1, m1)
-    print(long)
+    l = str(input("Введите где будет выполнятся задача: "))
+    h2 = int(input("Введите сколько часов планируется на дорогу: "))
+    m2 = int(input("Введите сколько минут планируется на дорогу"))
+    tt = datetime.time(h2, m2)
+    #st = 0 блок проверки на статус выполнения задачи быть ему не тут
+    #if st == 0:
+        #st1 = "Не выполнено"
+    #else:
+        #st1 = "Выполнено"
+
+# функция выполнено/не выполнено
+def markdone():
+    d1 = int(input("Введите айди задачи: "))
+    d2 = int(input("Введите статус задачи (1-выполнено, 2-не выполнено): "))
+
+# вывод выполненых задач
+def done():
+    pass
+
+# вывод не выполненых задач
+def notdone():
+    pass
+
+# функция интерфейса
 def inteface():
     a = int(input())
     if a == 1:
         registration()
     elif a == 2:
-        pass
+        account()
     elif a == 3:
-        pass
+        createtask()
     elif a == 4:
-        pass
+        markdone()
     elif a == 5:
-        pass
+        done()
     elif a == 6:
-        pass
+        notdone()
     elif a == 7:
         return a
     else:
@@ -95,4 +126,3 @@ def main():
         end = inteface()
     else:
         return 0
-createtask()
